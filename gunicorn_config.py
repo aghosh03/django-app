7fbc2a88a47e91ec11d951e34d6ed197ede432f4
aghosh03 <avikghosh03@gmail.com>
2023-03-17 00:01:38 -0700

    commit udpates

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import os
 
-BASE_DIR = Path(__file__).resolve().parent
-command = os.path.join(BASE_DIR,'bin/gunicorn')
-pythonpath = BASE_DIR
+BASE_DIR = str(Path(__file__).resolve().parent)
+command = str(os.path.join(BASE_DIR,'bin/gunicorn'))
+pythonpath =str(BASE_DIR)
 bind = '0.0.0.0:8000'
 workders =3
