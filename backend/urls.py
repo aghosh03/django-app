e776818e80e0a1db97da50d34ceef0d1823943cc
aghosh03 <avikghosh03@gmail.com>
2023-03-18 18:54:43 -0700

    refresh

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path, include
 from rest_framework import routers
 from .api import *
 from . import views
-from main.settings import FORCE_SCRIPT_NAME
 
 app_name = "backend"
 router = routers.DefaultRouter()
@@ -10,7 +9,7 @@ router.register(r'records', RecordViewSet, 'records')
 router.register(r'users', UserViewSet, 'users')
 
 urlpatterns = [
-     path(FORCE_SCRIPT_NAME,views.index, name="index"),
-     path(FORCE_SCRIPT_NAME+'settings/',views.settings, name="settings"),
-     re_path(FORCE_SCRIPT_NAME+'data/', include((router.urls)))
+     path('',views.index, name="index"),
+     path('settings/',views.settings, name="settings"),
+     re_path('data/', include((router.urls)))
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .settings import FORCE_SCRIPT_NAME
 
 urlpatterns = [
-    path(FORCE_SCRIPT_NAME, include("backend.urls")),
-    path(FORCE_SCRIPT_NAME+'users/', include("users.urls")),
-    path(FORCE_SCRIPT_NAME+'admin/', admin.site.urls),
+    path('',include("backend.urls")),
+    path('users/', include("users.urls")),
+    path('admin/', admin.site.urls),
 ]
